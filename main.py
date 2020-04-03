2b01bc2829502b490c590514d3062608e969846e
nianhua2 <1446013320@qq.com>
2020-04-03 22:53:25 +0800

    全量

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ time_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
 # 筛选出冒烟用例运行
 # pytest.main(["-m","smoke"])
 # 失败的用例的重运行2次，每次间隔5秒
-pytest.main(["-s", "-m", "smoke",
+'''pytest.main(["-s", "-m", "smoke",
              "--reruns", "2", "--reruns-delay", "5",
              "--junit-xml=OutPuts/reports/" + "auto_" + time_stamp + ".xml",
              "--html=OutPuts/reports/" + "auto_" + time_stamp + ".html",
-             "--alluredir=OutPuts/allure_reports"])
+             "--alluredir=OutPuts/allure_reports"])'''
 
-'''pytest.main(["-s",
+pytest.main(["-s",
               "--reruns", "2", "--reruns-delay", "5",
              "--junit-xml=OutPuts/reports/"+"auto_"+time_stamp+".xml",
              "--html=OutPuts/reports/"+"auto_"+time_stamp+".html",
-              "--alluredir=OutPuts/allure_reports"])'''
+              "--alluredir=OutPuts/allure_reports"])
